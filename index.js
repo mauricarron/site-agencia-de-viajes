7bda4c6646185ae619d5628f9f9fb2ccb3f67241
mdcarron <mauri.carron@gmail.com>
2021-02-20 10:35:54 -0300

    Add Pug dependencies and create views

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import router from "./routes/index.js";
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.set("view engine", "pug");
+
 app.use("/", router);
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Este es el homepage");
+  res.render("home");
+});
+router.get("/code", (req, res) => {
+  res.render("code");
+});
+router.get("/about", (req, res) => {
+  res.send("Photographer turned Developer");
+});
+router.get("/contact", (req, res) => {
+  res.send("Contact me @mdcarron");
 });
 
 export default router;
